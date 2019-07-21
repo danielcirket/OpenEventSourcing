dbe5cc08d94fb873867703c7f23526e53e5aed48
Dan Cirket <daniel.cirket@gmail.com>
2019-07-21 15:27:24 +0100

    Add `AddAzureServiceBus` extension method for service registration

---
+++ src/OpenEventSourcing.Azure.ServiceBus/Extensions/OpenEventSourcingBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using OpenEventSourcing.Azure.ServiceBus.Messages;
+using OpenEventSourcing.Azure.ServiceBus.Subscriptions;
+using OpenEventSourcing.Azure.ServiceBus.Topics;
+using OpenEventSourcing.Events;
+
+namespace OpenEventSourcing.Azure.ServiceBus.Extensions
+{
+    public static class OpenEventSourcingBuilderExtensions
+    {
+        public static IOpenEventSourcingBuilder AddAzureServiceBus(this IOpenEventSourcingBuilder builder, Action<ServiceBusOptions> optionsAction)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            builder.Services.AddSingleton<IEventBus, AzureServiceBus>();
+            builder.Services.AddScoped<IMessageFactory, DefaultMessageFactory>();
+            builder.Services.AddScoped<ISubscriptionClientFactory, DefaultSubscriptionClientFactory>();
+            builder.Services.AddScoped<ISubscriptionClientManager, DefaultSubscriptionClientManager>();
+            builder.Services.AddScoped<ITopicClientFactory, DefaultTopicClientFactory>();
+            builder.Services.AddScoped<ITopicMessageReceiver, DefaultTopicMessageReceiver>();
+            builder.Services.AddScoped<ITopicMessageSender, DefaultTopicMessageSender>();
+
+            return builder;
+        }
+    }
+}
