dad7317499410207a2659bb2c65bcd88f8515f02
Dan Cirket <daniel.cirket@gmail.com>
2020-03-10 11:29:14 +0000

    Add ability to supply connection string options to AddEntityFrameworkCoreSqlite

---
+++ src/OpenEventSourcing.EntityFrameworkCore.Sqlite/SqliteOptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace OpenEventSourcing.EntityFrameworkCore.Sqlite
+{
+    public class SqliteOptions
+    {
+        public string StoreConnectionString { get; internal set; }
+        public string ProjectionConnectionString { get; internal set; }
+        internal Action<SqliteDbContextOptionsBuilder> SqliteOptionsBuilder { get; set; }
+
+        public SqliteOptions UseStoreConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or empty.");
+
+            StoreConnectionString = connectionString;
+
+            return this;
+        }
+        public SqliteOptions UseProjectionConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or empty.");
+
+            ProjectionConnectionString = connectionString;
+
+            return this;
+        }
+        public SqliteOptions UseSqliteOptions(Action<SqliteDbContextOptionsBuilder> optionsAction = null)
+        {
+            SqliteOptionsBuilder = optionsAction;
+
+            return this;
+        }
+    }
+}
