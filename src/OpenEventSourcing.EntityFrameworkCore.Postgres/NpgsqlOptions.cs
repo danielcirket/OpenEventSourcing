2574bb79596b0a2b2f2a899dfabf2e936de7b281
Dan Cirket <daniel.cirket@gmail.com>
2020-03-10 11:29:41 +0000

    Add ability to supply connection string options to AddEntityFrameworkCorePostgres

---
+++ src/OpenEventSourcing.EntityFrameworkCore.Postgres/NpgsqlOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
+
+namespace OpenEventSourcing.EntityFrameworkCore.Postgres
+{
+    public class NpgsqlOptions
+    {
+        public string StoreConnectionString { get; internal set; }
+        public string ProjectionConnectionString { get; internal set; }
+        internal Action<NpgsqlDbContextOptionsBuilder> NpgsqlOptionsBuilder { get; set; }
+
+        public NpgsqlOptions UseStoreConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or empty.");
+
+            StoreConnectionString = connectionString;
+
+            return this;
+        }
+        public NpgsqlOptions UseProjectionConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentException($"'{nameof(connectionString)}' cannot be null or empty.");
+
+            ProjectionConnectionString = connectionString;
+
+            return this;
+        }
+        public NpgsqlOptions UseNpgsqlOptions(Action<NpgsqlDbContextOptionsBuilder> optionsAction = null)
+        {
+            NpgsqlOptionsBuilder = optionsAction;
+
+            return this;
+        }
+    }
+}
