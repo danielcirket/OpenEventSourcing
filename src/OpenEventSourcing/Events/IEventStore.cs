ac95cf0337b511102ae217ec5d8da71f96bb6c73
Dan Cirket <daniel.cirket@gmail.com>
2021-01-21 21:28:06 +0000

    Strongly type stream ids

--- src/OpenEventSourcing/Events/IEventStore.cs
+++ src/OpenEventSourcing/Events/IEventStore.cs
@@ -8,9 +8,9 @@ namespace OpenEventSourcing.Events
     public interface IEventStore
     {
         Task<Page> GetEventsAsync(long offset, CancellationToken cancellationToken = default);
-        Task<IEnumerable<IEventContext<IEvent>>> GetEventsAsync(string streamId, CancellationToken cancellationToken = default);
-        Task<IEnumerable<IEventContext<IEvent>>> GetEventsAsync(string streamId, long offset, CancellationToken cancellationToken = default);
-        Task SaveAsync(string streamId, IEnumerable<IEventContext<IEvent>> events, CancellationToken cancellationToken = default);
-        Task<long> CountAsync(string streamId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<IEventContext<IEvent>>> GetEventsAsync(StreamId streamId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<IEventContext<IEvent>>> GetEventsAsync(StreamId streamId, long offset, CancellationToken cancellationToken = default);
+        Task SaveAsync(StreamId streamId, IEnumerable<IEventContext<IEvent>> events, CancellationToken cancellationToken = default);
+        Task<long> CountAsync(StreamId streamId, CancellationToken cancellationToken = default);
     }
 }
---
+++ src/OpenEventSourcing/Events/StreamId.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OpenEventSourcing.Events
+{
+    public readonly struct StreamId : IEquatable<StreamId>
+    {
+        internal string Value { get; }
+
+        internal StreamId(string value)
+        {
+            Value = value;
+        }
+
+        public static StreamId New()
+        {
+            var value = Base64UrlIdGenerator.New();
+            
+            return new StreamId(value);
+        }
+        public static StreamId From(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException($"'{nameof(value)}' cannot be null or empty.", nameof(value));
+
+            return new StreamId(value);
+        }
+        
+        public bool Equals(StreamId other) => Value == other.Value;
+        public override bool Equals(object obj) => obj is StreamId other && Equals(other);
+        public override int GetHashCode() => Value.GetHashCode();
+        public override string ToString() => Value;
+
+        public static bool operator ==(StreamId left, StreamId right) => left.Equals(right);
+        public static bool operator !=(StreamId left, StreamId right) => !left.Equals(right);
+        public static implicit operator string(StreamId id) => id.Value;
+    }
+}
